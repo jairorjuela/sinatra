a0c4cc1fa592d9cc4de9185b0f50914cc1bb934f
jairOrjuela <genius85862@gmail.com>
2017-11-08 20:42:38 -0500

    modificacion reto 1

--- solution.rb
+++ solution.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 get '/' do
   @nombre = params[:nombre]
     if @nombre
-      "<h1> Hola #{@nombre}! </h1>"
+      "<h1>Hola #{@nombre}!</h1>"
     else
-      "Hola Desconocido"
+      "Hola desconocido!"
     end
 end
