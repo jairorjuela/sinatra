690eba3949fc830249b3c7cf052add81c9311f8c
jairOrjuela <genius85862@gmail.com>
2017-11-08 21:06:39 -0500

    reto 2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
-get '/' do
+# get '/' do
+#   @nombre = params[:nombre]
+#     if @nombre
+#       "<h1>Hola #{@nombre}!</h1>"
+#     else
+#       "Hola desconocido!"
+#     end
+# end
+
+get '/makers/:nombre' do
   @nombre = params[:nombre]
     if @nombre
-      "<h1>Hola #{@nombre}!</h1>"
+      "<h1>Hola #{@nombre.capitalize}!</h1>"
     else
       "Hola desconocido!"
     end
